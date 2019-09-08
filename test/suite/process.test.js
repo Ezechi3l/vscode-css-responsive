2502ec1b8fdcc75c5c9ea4f2c0abebaa015990f4
Ezechi3l <mrezechi3l@gmail.com>
2019-09-08 19:42:21 +0200

    Better definition on test

--- test/suite/process.test.js
+++ test/suite/process.test.js
@@ -10,56 +10,56 @@ suite('Process Test Suite', () => {
 	});
 
 	const process = new Process({});
-	test('if it works', () => {
+	test('It works', () => {
 		assert.equal(
 			'20',
 			process.run('width: 20/100', 'css').result,
 			"'20/100' should return 20, '" + process.run('width: 20/100', 'css').result + "' returned"
 		);
 	});
-	test('if space don\'t works', () => {
+	test('It don\'t works is there is space in the expression', () => {
 		assert.equal(
 			null,
 			process.run('100 /20', 'css').result,
 			"'100 /20' should return null, '" + process.run('100 /20', 'css').result + "' returned"
 		);
 	});
-	test('if worked on large line', () => {
+	test('it works in large line', () => {
 		assert.equal(
 			'6.521739',
 			process.run('	   width: 15/230', 'css').result,
 			"'15/230' should return 6.521739, '" + process.run('15/230', 'css').result + "' returned"
 		);
 	});
-	test('if it returns 6 digits maximum before the coma', () => {
+	test('It returns 6 digits maximum before the coma', () => {
 		assert.equal(
 			'6.521739',
 			process.run('15/230', 'css').result,
 			"'15/230' should return 6.521739, '" + process.run('15/230', 'css').result + "' returned"
 		);
 	});
-	test('if it trims digits before comma whom are zeros ', () => {
+	test('It trims digits before comma whom are zeros ', () => {
 		assert.equal(
 			'7.5',
 			process.run('15/200', 'css').result,
 			"'15/200' should return 7.5, '" + process.run('15/200', 'css').result + "' returned"
 		);
 	});
-	test('if numbers only don\'t matches', () => {
+	test('It doesn\'t work with number alone', () => {
 		assert.equal(
 			null,
 			process.run('width: 100', 'css').result,
 			"'width: 100' should return null, '" + process.run('width: 100', 'css').result + "' returned"
 		);
 	});
-	test('If calc don\'t interfere', () => {
+	test('It doesn\'t interfere with calc', () => {
 		assert.equal(
 			null,
 			process.run('width: calc(100', 'css').result,
 			"'width: calc(100' should return null, '" + process.run('width: calc(100', 'css').result + "' returned"
 		);
 	});
-	test('if infinity number works', () => {
+	test('It handles infinity number', () => {
 		assert.equal(
 			null,
 			process.run('100/0', 'css').result,
